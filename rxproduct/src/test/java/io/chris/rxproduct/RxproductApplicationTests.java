97f7d0993fddb24376c59d69c40ac996cf0b39ec
chris <cvaldezchavez@gmail.com>
2024-12-03 01:48:32 -0500

    add integration test

--- rxproduct/src/test/java/io/chris/rxproduct/RxproductApplicationTests.java
+++ rxproduct/src/test/java/io/chris/rxproduct/RxproductApplicationTests.java
@@ -1,13 +1,46 @@
 package io.chris.rxproduct;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.chris.rxproduct.model.Product;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class RxproductApplicationTests {
+
+    @Autowired
+    WebTestClient webTestClient;
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void createProduct() throws Exception {
+        Product product = Product.builder()
+                .name("monitor")
+                .enabled(true)
+                .build();
+        this.webTestClient.post().uri("/product")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(product)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Product.class);
+    }
 }
